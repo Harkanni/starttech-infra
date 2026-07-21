53d798860ad89a90f588b9132a04a19f6be2ab1d
harkanni <harkanzyempire001@gmail.com>
2026-07-21 13:57:08 +0100

    eks instance type change

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -82,7 +82,7 @@ resource "aws_eks_node_group" "starttech-node-group" {
   node_role_arn   = aws_iam_role.eks_node_role.arn
   subnet_ids      = var.private_subnet_ids
 
-  instance_types = ["t3.medium"]
+  instance_types = ["c7i-flex.large"]
 
   scaling_config {
     desired_size = var.node_desired_size
