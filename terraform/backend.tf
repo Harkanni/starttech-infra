9decc0901fe1dfcec4d26c2a84110f2af7301f0b
harkanni <harkanzyempire001@gmail.com>
2026-07-22 14:29:27 +0100

    fix workflow failing due to incorrect terraform formaatting

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "starttech-terraform-state-bucket-aws-cloud-provider" # Change to a unique bucket name
-    key            = "state/terraform.tfstate"
-    region         = "us-east-1"                        # Change to your AWS region
-    encrypt        = true
+    bucket  = "starttech-terraform-state-bucket-aws-cloud-provider" # Change to a unique bucket name
+    key     = "state/terraform.tfstate"
+    region  = "us-east-1" # Change to your AWS region
+    encrypt = true
   }
 }
