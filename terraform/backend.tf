2e0df23d4a0800b8874c7fbff13f569f238baa94
harkanni <harkanzyempire001@gmail.com>
2026-07-22 13:37:03 +0100

    backend file

---
+++ terraform/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "starttech-terraform-state-bucket" # Change to a unique bucket name
+    key            = "state/terraform.tfstate"
+    region         = "us-east-1"                        # Change to your AWS region
+    encrypt        = true
+  }
+}
