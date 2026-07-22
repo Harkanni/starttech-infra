3cf205b91a894855b69d295117088124ebbd0be1
harkanni <harkanzyempire001@gmail.com>
2026-07-22 13:54:27 +0100

    backend file

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "starttech-terraform-state-bucket" # Change to a unique bucket name
+    bucket         = "starttech-terraform-state-bucket-aws-cloud-provider" # Change to a unique bucket name
     key            = "state/terraform.tfstate"
     region         = "us-east-1"                        # Change to your AWS region
     encrypt        = true
